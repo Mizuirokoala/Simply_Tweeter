c664b02793fa4e62c9b137261f5eae3fa25e60c2
Damian <Mizuirokoala@gmail.com>
2018-09-25 06:40:15 +0200

    bean, Logindata and Session

---
+++ src/main/java/pl/mizuirokoala/bean/SessionManager.java
@@ -0,0 +1,15 @@
+package pl.mizuirokoala.bean;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpSession;
+
+@Component
+public class SessionManager {
+	public static HttpSession session() {
+		ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+		return attr.getRequest().getSession(true);
+	}
+}
